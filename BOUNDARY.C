929848be09b1b79d415558215467db9cfcb193ee
Pratiksha Sonawane <98272441+Pratiksha-S-Sonawane@users.noreply.github.com>
2023-09-23 13:26:22 +0530

    Add files via upload
    Flood fill algorithm is used to fill polygon with color.
    
    There are 4 connected and 8 Connected method to fill polygon 
    this means considering one pixel as a seed pixel and coloring it's 
    neighboring pixel ,in 4 connected 4 neighboring pixels are colored 
    and in 8 connected 8 neighboring pixels are colored.
    
    we alredy have a another type of polygon filling i.e. boundary fill
    In boundary fill we compare fill color with  boundary color
     
    But this methos is not effective in case of multi-colored boundaries
    thats why flood fill algorithm is used,
    
    In this algorithm old color is compared with new color as follows:
    
    c=getpixel(x,y);
    if(cl=new_c)
    
    getpixel(x,y) meto return the color of (x,y) pixel to c(variable for old color)
    then condition has been checked if this condition is true then and 
    then only new color is applied to (x,y) pixel by using:
    
    putpixel(x,y,new_c);
    
    putpixel() basically used to plot a pixel with given  color 
    
    after that i have used the 4 connected method to color the pixel

---
+++ BOUNDARY.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#include<graphics.h>
+
+void main()
+{
+ int gd=DETECT,gm;
+ clrscr();
+ initgraph(&gd,&gm,"..//BGI");
+ rectangle(50,50,200,200);
+ flood(55,55,5,15);
+
+ getch();
+}
+flood(x,y,n_clr,o_clr)
+{
+ int c;
+ c=getpixel(x,y);
+ if(c==o_clr)
+ {
+  delay(10);
+  putpixel(x,y,n_clr);
+  flood(x+1,y,n_clr,o_clr);
+  flood(x,y+1,n_clr,o_clr);
+  flood(x-1,y,n_clr,o_clr);
+  flood(x,y-1,n_clr,o_clr);
+
+ }
+ return 0;
+}
